802522baffaada81e89a9ccb4d1b0ee898e0237f
pnnjoroge <paulettenjoroge@gmail.com>
2026-03-01 17:53:00 -0500

    Completed Lab 2-10

---
+++ Unit2/Lesson2_10/Two Buttons/Two Buttons/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Two Buttons
+//
+//  Created by Paulette Njeri on 3/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func setTextButtonTapped(_ sender: Any) {
+        label.text = textField.text
+    }
+    
+    @IBAction func clearTextButtonTapped(_ sender: Any) {
+        textField.text = ""
+        label.text = ""
+    }
+}
+
